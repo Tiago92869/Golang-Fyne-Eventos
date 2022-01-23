d96eda7167967e59e9bfbd0f857735c115d13060
MARTINS <tiagomartinas2000@gmail.com>
2022-01-23 18:27:27 +0000

    hh

--- Testes/mark.go
+++ Testes/mark.go
@@ -3,34 +3,44 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func Window() {
+func Main() {
 	a := app.New()
 	w := a.NewWindow("teste")
 	w.Resize(fyne.NewSize(400, 400))
 
 	entry := widget.NewEntry()
 	entry.SetPlaceHolder("Enter your name")
-	entry.Resize(fyne.NewSize(200, 200))
+	entry.Resize(fyne.NewSize(250, 30))
 	entry.Move(fyne.NewPos(40, 50))
 
 	entry_email := widget.NewEntry()
 	entry_email.SetPlaceHolder("Enter your email")
-	entry_email.Resize(fyne.NewSize(200, 200))
+	entry_email.Resize(fyne.NewSize(250, 30))
 	entry_email.Move(fyne.NewPos(40, 100))
 
 	entry_address := widget.NewEntry()
 	entry_address.SetPlaceHolder("Enter your name")
-	entry_address.Resize(fyne.NewSize(200, 200))
+	entry_address.Resize(fyne.NewSize(250, 30))
 	entry_address.Move(fyne.NewPos(40, 150))
 
 	//button
 	btn_submit := widget.NewButton("Submit", nil)
-	entry.Resize(fyne.NewSize(200, 200))
-	entry.Move(fyne.NewPos(40, 50))
-	//show and run
+	btn_submit.Resize(fyne.NewSize(100, 30))
+	btn_submit.Move(fyne.NewPos(40, 200))
+
+	w.SetContent(
+
+		container.NewWithoutLayout(
+			entry,
+			entry_email,
+			entry_address,
+			btn_submit,
+		),
+	) //show and run
 	w.ShowAndRun()
 
 }
